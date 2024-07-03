caf4bf10d13a8ddc5474b09e28d4cbca6cd32c26
Shahed Al Massri <shahedalmassri@gmail.com>
2024-07-02 23:06:54 -0400

    Removed Computer and Player from functions folder

--- euchre-react/src/functions/Euchre/Computer.tsx
+++
@@ -1,8 +0,0 @@
-import { gamePhase } from "../../interfaces/gamePhase";
-import { gameInterface } from "../../interfaces/gameInterface";
-import { Hand } from "../../models/Hand";
-
-export function pickTrump(game: gameInterface) : gameInterface {
-    
-}
-  
