aca80adceb2bfda615c775f1d4d9f8d299013aa0
Michael Wood <mwood@data-crush.com>
2024-07-14 08:20:33 -0400

    add tests

---
+++ euchre-react/src/lib/euchre.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from "vitest";
+
+import { makeDeck, shuffle } from "./card.interface";
+import { ranks, suits } from "./euchre.interface";
+
+const euchreDeck = makeDeck(suits, ranks);
+
+test("make euchre deck", () => {
+  expect(euchreDeck.length).toBe(24);
+});
+
+test("shuffle deck", () => {
+  const shuffledDeck = shuffle(euchreDeck);
+
+  expect(shuffledDeck).not.toEqual(euchreDeck);
+});
