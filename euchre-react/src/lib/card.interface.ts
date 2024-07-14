bdcfd66bc7637020ad5a17735f54663d7d77cd0b
Michael Wood <mwood@data-crush.com>
2024-07-14 08:26:45 -0400

    use enums

--- euchre-react/src/lib/card.interface.ts
+++ euchre-react/src/lib/card.interface.ts
@@ -1,34 +1,35 @@
-export enum Suit {
+export enum Suits {
   DIAMONDS = "diamonds",
   SPADES = "spades",
   CLUBS = "clubs",
   HEARTS = "hearts",
 }
 
-export type Rank =
-  | "2"
-  | "3"
-  | "4"
-  | "5"
-  | "6"
-  | "7"
-  | "8"
-  | "9"
-  | "10"
-  | "J"
-  | "Q"
-  | "K"
-  | "A"
-  | "Joker";
+export enum Ranks {
+  TWO = "2",
+  THREE = "3",
+  FOUR = "4",
+  FIVE = "5",
+  SIX = "6",
+  SEVEN = "7",
+  EIGHT = "8",
+  NINE = "9",
+  TEN = "10",
+  JACK = "J",
+  QUEEN = "Q",
+  KING = "K",
+  ACE = "A",
+  JOKER = "Joker",
+}
 
 export interface Card<T> {
-  suit: Suit;
+  suit: Suits;
   rank: T;
 }
 
-export const makeDeck = <R>(suits: Suit[], ranks: Array<R>): Card<R>[] =>
+export const makeDeck = <R>(suits: Suits[], ranks: Array<R>): Card<R>[] =>
   suits.reduce(
-    (deck: Card<R>[], suit: Suit) =>
+    (deck: Card<R>[], suit: Suits) =>
       ranks.reduce(
         (deck: Card<R>[], rank: R) => deck.concat({ suit, rank } as Card<R>),
         deck
--- euchre-react/src/lib/euchre.interface.ts
+++ euchre-react/src/lib/euchre.interface.ts
@@ -1,15 +1,22 @@
-import { Card, Suit } from "./card.interface";
+import { Card, Ranks, Suits } from "./card.interface";
 import { Player } from "./player.interface";
 
 export type EuchreRank = "9" | "10" | "J" | "Q" | "K" | "A";
 
-export const ranks: EuchreRank[] = ["9", "10", "J", "Q", "K", "A"];
+export const ranks: EuchreRank[] = [
+  Ranks.NINE,
+  Ranks.TEN,
+  Ranks.JACK,
+  Ranks.QUEEN,
+  Ranks.KING,
+  Ranks.ACE,
+];
 
-export const suits: Suit[] = [
-  Suit.DIAMONDS,
-  Suit.CLUBS,
-  Suit.HEARTS,
-  Suit.SPADES,
+export const suits: Suits[] = [
+  Suits.DIAMONDS,
+  Suits.CLUBS,
+  Suits.HEARTS,
+  Suits.SPADES,
 ];
 
 export enum Phases {
