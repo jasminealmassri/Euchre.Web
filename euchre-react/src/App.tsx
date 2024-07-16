1e83f35206b6a936c5c6a6dcf32bc6a42314b542
Shahed Al Massri <shahedalmassri@gmail.com>
2024-07-15 20:58:49 -0400

    Made current trump in score component show as an icon instead of text

--- euchre-react/src/App.tsx
+++ euchre-react/src/App.tsx
@@ -3,13 +3,14 @@ import TableComponent from './components/TableComponent'
 import ScoreComponent from './components/ScoreComponent'
 import EuchreComponent from './components/EuchreComponent';
 
+//import Clubs from './assets/images/Suits/Clubs.png'
+
 
 function App() {
 
  
   return (
     <>
-     
         <div className="game">
           <EuchreComponent>
             <ScoreComponent/>
