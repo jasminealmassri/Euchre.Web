9a63e50a19cfafe06a8d8289935f5853fe0cc1ea
Michael Wood <mwood@data-crush.com>
2024-07-20 05:50:49 -0400

    add thunk type

--- euchre-react/src/lib/store.ts
+++ euchre-react/src/lib/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 
 import euchreReducer from "./euchre-slice";
 
@@ -10,3 +10,9 @@ export const store = configureStore({
 
 export const AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
