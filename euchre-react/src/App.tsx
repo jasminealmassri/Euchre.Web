4df05a444ca6009e53c449de76f1ba1b9988224f
Jasmine Al Massri <almassri.jasmine@gmail.com>
2024-07-30 20:45:53 -0400

    Merged redux refactor

--- euchre-react/src/App.tsx
+++ euchre-react/src/App.tsx
@@ -3,10 +3,19 @@ import EngineDemo from "./components/EngineDemo";
 
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+import EuchreComponent from "./components/EuchreComponent";
+import ScoreComponent from "./components/ScoreComponent";
+import TableComponent from "./components/TableComponent";
 
 function App() {
   return (
     <Provider store={store}>
+       <div className="game">
+        <EuchreComponent>
+          <ScoreComponent />
+          <TableComponent />
+        </EuchreComponent>
+      </div>
       <div>
         <EngineDemo />
       </div>
