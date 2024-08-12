e243c553ac116625e261551e6a212cfa37653fb0
Michael Wood <mwood@data-crush.com>
2024-08-12 06:47:46 -0400

    proper player order

--- euchre-react/src/state/thunks/euchre/call-trump.ts
+++ euchre-react/src/state/thunks/euchre/call-trump.ts
@@ -1,22 +1,11 @@
 import { Phase, PlayingCardSuit } from "../..";
-import {
-  nextPlayer,
-  setCurrentPlayer,
-  setRole,
-  setTrump,
-  transitionToPhase,
-} from "../../reducers/euchre";
-import { selectDealerPointer } from "../../selectors/euchre";
+import { setRole, setTrump, transitionToPhase } from "../../reducers/euchre";
 import { AppThunk } from "../../store";
 
 export const callTrump =
   (playerPointer: number, trump: PlayingCardSuit): AppThunk =>
-  (dispatch, getState) => {
-    const dealer = selectDealerPointer(getState().euchre);
-
+  (dispatch) => {
     dispatch(setTrump(trump));
     dispatch(setRole({ makerPointer: playerPointer }));
-    dispatch(setCurrentPlayer(dealer));
     dispatch(transitionToPhase(Phase.DECLARING));
-    dispatch(nextPlayer());
   };
--- euchre-react/src/state/thunks/euchre/declare.ts
+++ euchre-react/src/state/thunks/euchre/declare.ts
@@ -1,13 +1,23 @@
 import { Phase } from "../..";
-import { benchPlayer, transitionToPhase } from "../../reducers/euchre";
+import {
+  benchPlayer,
+  nextPlayer,
+  setCurrentPlayer,
+  transitionToPhase,
+} from "../../reducers/euchre";
+import { selectDealerPointer } from "../../selectors/euchre";
 import { AppThunk } from "../../store";
 
 export const declare =
   (declaration: "alone" | null = null): AppThunk =>
-  (dispatch) => {
+  (dispatch, getState) => {
+    const dealer = selectDealerPointer(getState().euchre);
+
     if (declaration === "alone") {
       dispatch(benchPlayer());
     }
 
     dispatch(transitionToPhase(Phase.PLAYING_TRICKS));
+    dispatch(setCurrentPlayer(dealer));
+    dispatch(nextPlayer());
   };
